0cc11bcdbba60ceaa3e616fb7396fb99c3c3f647
Owen Smith <ojman101@gmail.com>
2020-07-29 14:08:56 +0100

    [frame-graph] Finish vertex buffer support

--- include/inexor/vulkan-renderer/octree_vertex.hpp
+++ include/inexor/vulkan-renderer/octree_vertex.hpp
@@ -1,26 +1,14 @@
 #pragma once
 
-#include <glm/glm.hpp>
-#include <vulkan/vulkan_core.h>
-
-#include <vector>
+#include <glm/vec3.hpp>
 
 namespace inexor::vulkan_renderer {
 
-// TODO: Generalize this setup using a builder pattern!
-class OctreeVertex {
-public:
-    glm::vec3 pos;
+struct OctreeVertex {
+    glm::vec3 position;
     glm::vec3 color;
 
-    OctreeVertex(glm::vec3 pos, glm::vec3 color);
-
-    [[nodiscard]] static VkVertexInputBindingDescription get_vertex_binding_description();
-
-    /// @note You should use the format where the amount of color channels matches the number of components in the
-    /// shader data type. It is allowed to use more channels than the number of components in the shader, but they will
-    /// be silently discarded.
-    [[nodiscard]] static std::vector<VkVertexInputAttributeDescription> get_attribute_binding_description();
+    OctreeVertex(glm::vec3 position, glm::vec3 color) : position(position), color(color) {}
 };
 
 } // namespace inexor::vulkan_renderer
